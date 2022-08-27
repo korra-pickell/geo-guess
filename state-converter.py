1bce470a59c604ca755148b982b67dcad0d90fcc
Korra Pickell <ronanpickell@gmail.com>
2022-08-27 10:08:53 -0500

    build_folders() create subfolder if doesn't exist

--- state-converter.py
+++ state-converter.py
@@ -1,4 +1,4 @@
-import time, shutil
+import time, shutil, os
 from geopy.geocoders import Nominatim
 from progress.bar import Bar
 
@@ -26,14 +26,28 @@ def get_states():
         dfile.close()
     bar.finish()
 
+def check_folder_exists(state):
+    parent = r'E:\DATA\GEO-GUESS\states\Image Classes'
+    subfolders = os.listdir(parent)
+    return state in subfolders
 
 def build_folders():
     c_path = r'E:\DATA\GEO-GUESS\states\coords_states.txt'
     coord_lines = open(c_path).readlines()
     for line in coord_lines:
         parts = line.split(',')
-        im_path,state = parts[0],parts[-1]
-        print(im_path,state)
+        im_path_src,state = parts[0],parts[-1].strip('\n')
+        img_name = parts[0].split('\\')[-1]
+        im_path_tar = os.path.join(r'E:\DATA\GEO-GUESS\states\Image Classes',state,img_name)
+        
+        if check_folder_exists(state):
+            shutil.copy(im_path_src,im_path_tar)
+        else:
+            try: 
+                os.mkdir(os.path.join(r'E:\DATA\GEO-GUESS\states\Image Classes',state))
+                shutil.copy(im_path_src,im_path_tar)
+            except OSError:
+                print("couldn't make folder "+state)
         s = input('...')
 
 build_folders()
